808d323380544b0e07d0bfd954172c7be7a4cd0f
Mark Otweyo <markotweyo99@gmail.com>
2021-10-23 00:43:34 +0300

    Adding Shopping Product List UI

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+import Product from './Product'
+import {popularProducts} from "../data";
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
 
 const Products = () => {
     return (
-        <div>
-            Products
-        </div>
+        <Container>
+            {popularProducts.map((item)=> (
+                <Product item={item} key={item.id}/>
+            ))}
+            
+        </Container>
     )
 }
 
