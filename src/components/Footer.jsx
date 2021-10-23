1a83e6c40504a46b986e83e8c67c4600275f7f00
Mark Otweyo <markotweyo99@gmail.com>
2021-10-23 03:17:18 +0300

    Adding Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,124 @@
+import { Email, Facebook, Instagram, Phone, Room, Twitter } from '@material-ui/icons'
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+
+`
+const Logo = styled.h1`
+    font-weight: bold;
+`
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    cursor: pointer;
+`
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 5px;
+`
+    
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Payment = styled.img`
+    width:50%;
+`
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <Container>
+            <Left>
+                <Logo>DUKA.</Logo>
+                <Desc>
+                Duka is your number one Online Shopping solution. You can purchase all your mobile phones, tablets, computers & laptops, women's fashion, men's fashion and more online and have them delivered to you. Duka has payment options that suit everyone, and we have a payment-on-delivery option for extra convenience.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact Us</Title>
+                <ContactItem>
+                    <Room style={{marginRight: "10px"}}/> 
+                    Nairobi, Kenya
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{marginRight: "10px"}}/> 
+                    +254 717 000 000
+                </ContactItem>
+                <ContactItem>
+                    <Email style={{marginRight: "10px"}}/>
+                    contact@duka.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+            </Right>
+        </Container>
     )
 }
 
