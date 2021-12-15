1ef2c875dab186010a492865899eb2e3145a28df
Mark Otweyo <markotweyo99@gmail.com>
2021-12-16 00:20:11 +0300

    Added: Ability to persist data with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Success from './pages/Success';
+import { useSelector } from 'react-redux';
 
 
 
 const App = () => {
-  
+  const user = useSelector(state => state.user.currentUser)
   
   return (
     <BrowserRouter>
